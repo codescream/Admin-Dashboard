73ff25bad45c6c936d86a9ccd2e79fd23d9ac6c2
ecclesia <ogilom@gmail.com>
2024-04-17 16:48:49 -0400

    complete deals plotting feature

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,6 +1,5 @@
 import { CalendarOutlined } from '@ant-design/icons';
 import { Badge, Card, List } from 'antd';
-import React, { useState } from 'react'
 import { Text } from '../text';
 import UpcomingEventsSkeleton from '../skeleton/upcoming-events';
 import { getDate } from '@/utilities/helpers';
@@ -9,8 +8,7 @@ import { DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY } from '@/graphql/queries';
 import dayjs from 'dayjs';
 
 const UpcomingEvents = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const { data, isLoading: eventsLoading } = useList({
+  const { data, isLoading } = useList({
     resource: 'events',
     pagination: { pageSize: 5 },
     sorters: [
