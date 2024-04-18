24af484d68ee2d10eddf71664109725deb9fa0ed
ecclesia <ogilom@gmail.com>
2024-04-18 11:29:12 -0400

    add latest activity feature

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import UpcomingEvents from "./home/upcoming-events";
 import DealsChart from "./home/deals-charts";
 import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
 import DashboardTotalCountCard from "./home/total-count-card";
+import DashboardLatestActivities from "./home/latest-activities";
 
 export {
   UpcomingEvents,
   DealsChart,
   UpcomingEventsSkeleton,
-  DashboardTotalCountCard
+  DashboardTotalCountCard,
+  DashboardLatestActivities,
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import { DashboardTotalCountCard, DealsChart, UpcomingEvents } from "@/components"
+import { DashboardLatestActivities, DashboardTotalCountCard, DealsChart, UpcomingEvents } from "@/components"
 import { DASHBOARD_TOTAL_COUNTS_QUERY } from "@/graphql/queries"
 import { DashboardTotalCountsQuery } from "@/graphql/types"
 import { useCustom } from "@refinedev/core"
@@ -20,36 +20,36 @@ export const Home = () => {
           marginTop: '32px'
         }}
       >
-          <Col
-            xs={24}
-            sm={24}
-            xl={8}
-          >
-            <DashboardTotalCountCard 
-              resource="companies"
-              isLoading={isLoading}
-              totalCount={data?.data.companies.totalCount}
-            />
-          </Col>
-          <Col
-            xs={24}
-            sm={24}
-            xl={8}
-          ><DashboardTotalCountCard 
-            resource="contacts"
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+        >
+          <DashboardTotalCountCard 
+            resource="companies"
             isLoading={isLoading}
-            totalCount={data?.data.contacts.totalCount}
-          /></Col>
-          <Col
-            xs={24}
-            sm={24}
-            xl={8}
-          ><DashboardTotalCountCard 
-            resource="deals"
-            isLoading={isLoading}
-            totalCount={data?.data.deals.totalCount}
-          /></Col>
-        </Row>
+            totalCount={data?.data.companies.totalCount}
+          />
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+        ><DashboardTotalCountCard 
+          resource="contacts"
+          isLoading={isLoading}
+          totalCount={data?.data.contacts.totalCount}
+        /></Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+        ><DashboardTotalCountCard 
+          resource="deals"
+          isLoading={isLoading}
+          totalCount={data?.data.deals.totalCount}
+        /></Col>
+      </Row>
       <Row
         gutter={[32, 32]}
         style={{
@@ -75,6 +75,18 @@ export const Home = () => {
           }}
         >
           <DealsChart />
+        </Col>
+      </Row>
+      <Row
+        gutter={[32, 32]}
+        style={{
+          marginTop: '32px'
+        }}
+      >
+        <Col
+          xs={24}
+        >
+          <DashboardLatestActivities />
         </Col>
       </Row>
     </div>
